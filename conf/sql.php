19d83f39037764576b18e262224a0fe589906754
Soldy <Soldy@h.7>
2016-04-10 05:01:27 +0000

    add sqlconfig

--- conf/sql.php
+++ conf/sql.php
@@ -1,4 +1,5 @@
 <?php
 
-
-
+$SUPERGLOBAL['config']['sql']['password']="123456";
+$SUPERGLOBAL['config']['sql']['user']="uploader";
+$SUPERGLOBAL['config']['sql']['db']="uploader";
--- include/config.php
+++ include/config.php
@@ -1,2 +1,12 @@
 <?php
 
+$SUPERGLOBAL['config']['sql']=[];
+require_once '../conf/sql.php';
+
+$SUPERGLOBAL['config']['file']=[];
+require_once '../conf/file.php';
+
+$SUPERGLOBAL['config']['upload']=[];
+require_once '../conf/upload.php';
+
+
