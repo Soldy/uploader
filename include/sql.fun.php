771c5e931c2e5549d7280b73f6bd4583db7fa306
Soldy <testa.a.tapos@gmail.com>
2016-04-10 07:40:55 +0000

    add sql function

---
+++ include/sql.fun.php
@@ -0,0 +1,43 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ * 
+ * i do this with security reasons
+ * 
+ * 
+ */
+
+class sql {
+    
+    public function insert(){
+        
+    } 
+
+    public function update(){
+        
+    } 
+    
+    public function select(){
+        
+    }
+    
+    public function connect(){
+        
+    }
+
+    public function getSerial($name){ // inside serializer. Better if we do with care
+        $serial = 0;
+        return $serial;
+    }    
+    
+    protected function __construct (){
+    }
+    
+    protected function __destruct() {
+     
+    }
+    
+}
