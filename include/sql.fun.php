a58011537609d8742dc496f169688df4fa1e880c
Soldy <testa.a.tapos@gmail.com>
2016-04-14 21:12:33 +0000

    add sql function...

--- include/sql.fun.php
+++ include/sql.fun.php
@@ -11,9 +11,13 @@
  */
 
 class sql {
+    private $sql ;
+    
+    private $config = [];
+    
     
     public function insert(){
-        
+        #("INSERT INTO test(id) VALUES (1), (2), (3)")) 
     } 
 
     public function update(){
@@ -21,19 +25,39 @@ class sql {
     } 
     
     public function select(){
-        
+    }
+    
+    public function query ($query) {
+        $res = $this->sql->query($query);
+        while($row = $res->fetch_array()){
+            $rows[] = $row;
+        }
+        $res->close();
+        return $rows;    
+    }  
+    
+    public function security($string){
+        return htmlspecialchars($string, ENT_QUOTES);
     }
     
     public function connect(){
-        
+        $this->sql = new mysqli($this->config['host'], $this->config["user"], $this->config["password"], $this->config["database"]);
+        if ($this->sql->connect_errno) die ("die die die fucking process just died!");
     }
 
     public function getSerial($name){ // inside serializer. Better if we do with care
+        $name=$this->security($name);
+        $result = $this->query("SELECT value FROM `serial` WHERE name = '". $name. "' ");
         $serial = 0;
+        $this->query("INSERT INTO `serial` (`name`, `value`) VALUES ('". $name. "', '1')");
+        $this->query("UPDATE `serial` SET `value` = '2' WHERE `name` = '". $name. "'");
         return $serial;
     }    
     
     protected function __construct (){
+        global $SUPERGLOBAL;
+        $this->config=$SUPERGLOBAL['config']['sql'];
+        $this->connect();
     }
     
     protected function __destruct() {
