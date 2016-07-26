70760ec0c1a9048ad86638029a961f24bfa1ef88
Soldy <testa.a.tapos@gmail.com>
2016-07-26 13:17:17 +0000

    sql normalization

--- include/sql.fun.php
+++ include/sql.fun.php
@@ -11,8 +11,12 @@
  */
 
 class SQL {
-    protected $sql ;
+    public $sql ;
+    public $config ;
     
+    public function timeStamp(){
+        return round(microtime(true));
+    }
     public function insert(){
         #("INSERT INTO test(id) VALUES (1), (2), (3)")) 
     } 
@@ -26,11 +30,16 @@ class SQL {
     
     public function query ($query) {
         $res = $this->sql->query($query);
+        if($res == false) return [""];
         while($row = $res->fetch_array()){
             $rows[] = $row;
         }
 //        $res->close();
         return $rows;    
+    }  
+
+    public function justquery ($query) {       
+        $res = $this->sql->multi_query($query);   
     }  
     
     public function security($string){
@@ -39,7 +48,7 @@ class SQL {
     
     protected function connect(){
         global $SUPERGLOBAL;
-        $this->sql = new mysqli('127.0.0.1', $SUPERGLOBAL['config']['sql']['user'], $SUPERGLOBAL['config']['sql']['password'], $SUPERGLOBAL['config']['sql']['db']);
+        $this->sql = new mysqli('127.0.0.1', $this->config['user'], $this->config['password'], $this->config['db']);
         if ($this->sql->connect_errno){
             error_log("mysql connect error ",1,"ld@bookluk.com");
             die ("die die die fucking process just died!");
@@ -48,32 +57,34 @@ class SQL {
 
     public function getSerial($name){ // inside serializer. Better if we do with care
         $name=$this->security($name);
-        $result = $this->query("SELECT value FROM `serial` WHERE name = '". $name. "' ");
+        $result = $this->query("SELECT value FROM `". $this->config['db']. "`.`serial` WHERE name = '". $name. "' ");
         if(isset($result[0]['value'])){ // future securit check
             $serial = $result[0]['value']+1;
-            $this->query("UPDATE `serial` SET `value` = '". $serial ."' WHERE `name` = '". $name. "'");            
+            $this->justquery("UPDATE `". $this->config['db']. "`.`serial` SET `value` = '". $serial ."' WHERE `name` = '". $name. "'");            
         }else{
             $serial = 0;
-            $this->query("INSERT INTO `serial` (`name`, `value`) VALUES ('". $name. "', '0')");
+            $this->justquery("INSERT INTO `". $this->config['db']. "`.`serial` (`name`, `value`) VALUES ('". $name. "', '0')");
         }
         return $serial;
     }    
 
    
-    public function createFile($size, $pieces, $type){ // inside serializer. Better if we do with care
+    public function createFile($size, $pieces, $type, $name){ // inside serializer. Better if we do with care
         $fileId=$this->getSerial("file");
-        $this->query("INSERT INTO `". $this->database. "`.`fileList` (`id`, `status`, `creationTimestamp`, `size`, `type`, `filePieceNumber`, `filePieceNumberStatus`, `uploadFinishedStamp`, `access`) VALUES ('". $fileId. "', 'new', '". microtime()."', '". $size. "', '".$type."', '". $pieces."', 'new', '0', 'public')");    
-        $sql="INSERT INTO `". $this->database. "`.`filePieces` (`id`, `fileId`, `status`) VALUES";
+        $this->justquery("INSERT INTO `". $this->config['db']. "`.`fileList` (`id`, `status`, `creationTimestamp`, `size`, `type`, `filePieceNumber`, `filePieceNumberStatus`, `uploadFinishedStamp`, `access`, `fileName`) VALUES ('". $fileId. "', 'new', '". $this->timeStamp(). "', '". $size. "', '".$type."', '". $pieces."', 'new', '0', 'public', '". $name. "')");    
+        $sql="INSERT INTO `". $this->config['db']. "`.`filePieces` (`id`, `fileId`, `status`) VALUES ";
         for ($i=0;$i<$pieces;$i++){
             if ($i>0) $sql.=", ";
             $sql.="('". $i. "', '". $fileId. "', 'new')";
         }
-        $this->query($sql);
+        error_log($sql);
+        $this->justquery($sql);
+        error_log($this->sql->error);
         return $fileId;
     }    
 
     public function getFileType($fileId){ // inside serializer. Better if we do with care
-        $result = $this->query("SELECT `". $this->database. "`.`fileList` FROM `serial` WHERE id = '". $this->security($fileId). "' ");      
+        $result = $this->query("SELECT * FROM `". $this->config['db']. "`.`fileList` WHERE id = '". $this->security($fileId). "' ");      
         if(isset($result[0]['type'])){
                 return $result[0]['type'];
         }else{
@@ -82,7 +93,7 @@ class SQL {
     }    
 
     public function getFilePieceNumber($fileId){ // inside serializer. Better if we do with care
-        $result = $this->query("SELECT `". $this->database. "`.`fileList` FROM `serial` WHERE id = '". $this->security($fileId). "' ");      
+        $result = $this->query("SELECT * FROM `". $this->config['db']. "`.`fileList` WHERE id = '". $this->security($fileId). "' ");      
         if(isset($result[0]['type'])){
                 return $result[0]['type'];
         }else{
@@ -92,12 +103,14 @@ class SQL {
     
     
     public function checkFileProcess($fileId){ // inside serializer. Better if we do with care
-        $result = $this->query("SELECT `". $this->database. "`.`fileList` FROM `serial` WHERE id = '". $this->security($fileId). "' ");      
+            error_log("-1.6");
+        $result = $this->query("SELECT * FROM `". $this->config['db']. "`.`fileList` WHERE id = '". $this->security($fileId). "' ");      
+            error_log("-1.4");
         if(isset($result[0]['status'])){
             if($result[0]['status'] == "process"){
                 return true;
             }elseif($result[0]['status']=="new"){
-                $this->query("UPDATE `". $this->database. "`.`fileList` SET `status` = 'process' WHERE `id` = '".  $this->security($fileId). "'" );          
+                $this->justquery("UPDATE `". $this->config['db']. "`.`fileList` SET `status` = 'process' WHERE `id` = '".  $this->security($fileId). "'" );          
                 return true;
             }else{
                 return false;
@@ -108,25 +121,25 @@ class SQL {
     }
     
     public function checkFileFinnished($fileId){ // inside serializer. Better if we do with care
-        $resultFile = $this->query("SELECT `". $this->database. "`.`fileList` FROM `serial` WHERE id = '". $this->security($fileId). "' ");    
-        $result = $this->query("SELECT`". $this->database. "`.`filePieces`  FROM `serial` WHERE `fileId` = '".  $this->security($fileId). "' AND `status` = 'ok' ");
-        if($resultFile[0]['filePieceNumber'] == count($result)) return $resultFile[0]['filePieceNumber'];
+        $resultFile = $this->query("SELECT * FROM `".  $this->config['db']. "`.`fileList` WHERE id = '". $this->security($fileId). "' ");    
+        $result = $this->query("SELECT * FROM `". $this->config['db']. "`.`filePieces` WHERE `fileId` = '".  $this->security($fileId). "' AND `status` = 'ok' ");
+        if($resultFile[0]['filePieceNumber'] == count($result)) return count($result);
         return false;
     }  
     
     
     public function setFileOk($fileId){ // inside serializer. Better if we do with care
-        $this->query("UPDATE `". $this->database. "`.`fileList` SET `status` = 'ok',`uploadFinishedStamp` = '". microtime(). "', WHERE `id` = '".  $this->security($fileId). "'" );  
+        $this->justquery("UPDATE `".  $this->config['db']. "`.`fileList` SET `status` = 'ok',`uploadFinishedStamp` = '". $this->timeStamp(). "', WHERE `id` = '".  $this->security($fileId). "'" );  
     }  
     
     
-    public function setFilePiecesTry($fileId, $piece){ // inside serializer. Better if we do with care
-        $result = $this->query("SELECT`". $this->database. "`.`filePieces`  FROM `serial` WHERE `fileId` = '".  $this->security($fileId). "' AND `id` = '".  $this->security($piece) . "'");      
+    public function checkFilePieces($fileId, $piece){ // inside serializer. Better if we do with care
+        $result = $this->query("SELECT * FROM `". $this->config['db']. "`.`filePieces`  WHERE `fileId` = '".  $this->security($fileId). "' AND `id` = '".  $this->security($piece) . "'");      
         if(isset($result[0]['status'])){
             if($result[0]['status'] == "try"){
                 return true;
             }elseif($result[0]['status']=="new"){
-                $this->setFilePiecesTry($fileId, $piece);
+                $this->setFilePieceTry($fileId, $piece);
                 return true;
             }else{
                 return false;
@@ -137,16 +150,18 @@ class SQL {
     } 
     
     public function setFilePieceTry($fileId, $piece){ // inside serializer. Better if we do with care
-        $this->query("UPDATE `". $this->database. "`.`filePieces` SET `status` = 'try' WHERE `fileId` = '".  $this->security($fileId). "' AND `id` = '".  $this->security($piece) . "'");
+        $this->justquery("UPDATE `". $this->config['db']. "`.`filePieces` SET `status` = 'try' WHERE `fileId` = '".  $this->security($fileId). "' AND `id` = '".  $this->security($piece) . "'");
     }   
     
     public function setFilePieceOk($fileId, $piece){ // inside serializer. Better if we do with care
-        $this->query("UPDATE `". $this->database. "`.`filePieces` SET `status` = 'ok' WHERE `fileId` = '".  $this->security($fileId). "' AND `id` = '".  $this->security($piece) . "'"); 
+        $this->justquery("UPDATE `". $this->config['db']. "`.`filePieces` SET `status` = 'ok' WHERE `fileId` = '".  $this->security($fileId). "' AND `id` = '".  $this->security($piece) . "'"); 
     }        
     
     
     
     public function __construct (){
+        global $SUPERGLOBAL;
+        $this->config=$SUPERGLOBAL['config']['sql'];
         $this->connect();
     }
     
